a4413ba65d6d90b86de36e7f1fab1aa137ada2cf
Rami <rami.gamal.mahmoud@gmail.com>
2024-08-07 15:21:20 +0300

    add HasMessageObjet

---
+++ AinAlAtaaFoundation.Shared/HasMessageObject.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AinAlAtaaFoundation.Shared
+{
+    public partial class HasMessageObject : ObservableObject
+    {
+        public bool HasMessage => !string.IsNullOrEmpty(Message);
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(HasMessage))]
+        private string _message;
+
+        public void Clear() => Message = "";
+    }
+}
