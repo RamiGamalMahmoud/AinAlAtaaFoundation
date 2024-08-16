4f888da5838fa364f6a39c9f9c07a898be1fae21
Rami <rami.gamal.mahmoud@gmail.com>
2024-08-16 19:38:58 +0300

    remove un necessary Save method

--- AinAlAtaaFoundation.Features.Management/FeaturedPointsManagement/Editor/ViewModelEditorBase.cs
+++ AinAlAtaaFoundation.Features.Management/FeaturedPointsManagement/Editor/ViewModelEditorBase.cs
@@ -16,7 +16,6 @@ namespace AinAlAtaaFoundation.Features.Management.FeaturedPointsManagement.Edito
             DataModel = new FeaturedPointDataModel(featuredPoint);
             HasChangesObject = new Shared.Helpers.HasChangesObject(SaveCommand.NotifyCanExecuteChanged);
             DataModel.PropertyChanged += DataModel_PropertyChanged;
-            _editType = featuredPoint is null ? EditType.Create : EditType.Update;
         }
 
         public override bool CanSave() => HasChangesObject.HasChanges && DataModel.IsValid;
@@ -26,38 +25,8 @@ namespace AinAlAtaaFoundation.Features.Management.FeaturedPointsManagement.Edito
             Districts = await _mediator.Send(new Shared.Commands.Generic.GetAllCommand<District>());
         }
 
-        protected override async Task Save()
-        {
-            if (_editType is EditType.Create)
-            {
-                FeaturedPoint featuredPoint = await _mediator.Send(new CommandHandlers.Create.Command(DataModel));
-                if (featuredPoint is null)
-                {
-                    _messenger.Send(new Notifications.FailerNotification("فشل في عملية الحفظ , بيانات مكررة!"));
-                    return;
-                }
-                else
-                {
-                    _messenger.Send(new Messages.EntityCreated<FeaturedPoint>(featuredPoint));
-                    _messenger.Send(new Notifications.SuccessNotification("تمت عملية الحفظ بنجاح"));
-                }
-            }
-            else if (_editType is EditType.Update)
-            {
-                if (!(await _mediator.Send(new CommandHandlers.Update.Command(DataModel))))
-                {
-                    _messenger.Send(new Notifications.FailerNotification("فشل في عملية الحفظ , بيانات مكررة!"));
-                    return;
-                }
-                _messenger.Send(new Messages.EntityUpdated<FeaturedPoint>(DataModel.Model));
-                _messenger.Send(new Notifications.SuccessNotification("تمت عملية الحفظ بنجاح"));
-            }
-        }
-
         [ObservableProperty]
         IEnumerable<District> _districts;
-
-        private readonly EditType _editType;
 
     }
 }
