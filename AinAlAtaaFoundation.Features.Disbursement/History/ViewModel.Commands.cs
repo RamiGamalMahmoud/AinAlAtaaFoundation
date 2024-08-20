816eb86cf03ccfebff69ce30e2601122de1953d4
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-08-20 19:34:17 +0300

    split Disbursement.History.ViewModel into partial files

---
+++ AinAlAtaaFoundation.Features.Disbursement/History/ViewModel.Commands.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
+
+namespace AinAlAtaaFoundation.Features.DisbursementManagement.History
+{
+    internal partial class ViewModel
+    {
+        [RelayCommand]
+        private async Task Filter()
+        {
+            Disbursements = await _mediator.Send(new Shared.Commands.Disbursements.GetHistory(
+                Clan,
+                Branch,
+                BranchRepresentative,
+                District,
+                DistrictRepresentative,
+                FamilyType,
+                SocialStatus,
+                OrphanType));
+        }
+    }
+}
--- AinAlAtaaFoundation.Features.Disbursement/History/ViewModel.cs
+++ AinAlAtaaFoundation.Features.Disbursement/History/ViewModel.cs
@@ -1,12 +1,21 @@
 ﻿using AinAlAtaaFoundation.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
-using System.Collections.Generic;
+using MediatR;
+using System.Threading.Tasks;
 
 namespace AinAlAtaaFoundation.Features.DisbursementManagement.History
 {
-    internal partial class ViewModel : ObservableObject
+    internal partial class ViewModel(IMediator mediator) : ObservableObject
     {
-        [ObservableProperty]
-        private IEnumerable<Disbursement> _disbursements;
+        public async Task LoadDataAsync()
+        {
+            Clans = await _mediator.Send(new Shared.Commands.Generic.GetAllCommand<Clan>());
+            FamilyTypes = await _mediator.Send(new Shared.Commands.Generic.GetAllCommand<FamilyType>());
+            SocialStatuses = await _mediator.Send(new Shared.Commands.Generic.GetAllCommand<SocialStatus>());
+
+            Districts = await _mediator.Send(new Shared.Commands.Generic.GetAllCommand<District>());
+        }
+
+        private readonly IMediator _mediator = mediator;
     }
 }
