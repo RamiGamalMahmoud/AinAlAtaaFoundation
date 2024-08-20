d945d2b25fe9373ccfa324d0a77c3115a35bcd7b
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-08-20 19:29:44 +0300

    add Item component

---
+++ AinAlAtaaFoundation.Features.Disbursement/Create/Item.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AinAlAtaaFoundation.Features.DisbursementManagement.Create
+{
+    public partial class Item : UserControl
+    {
+        public Item()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+        private static readonly DependencyProperty _titleProperty = DependencyProperty.Register(
+            nameof(Title),
+            typeof(string),
+            typeof(Item),
+            new PropertyMetadata(null));
+
+        public string Value
+        {
+            get;
+            set;
+        }
+        private static readonly DependencyProperty _valueProperty = DependencyProperty.Register(
+            nameof(Value),
+            typeof(string),
+            typeof(Item),
+            new PropertyMetadata(null));
+    }
+}
