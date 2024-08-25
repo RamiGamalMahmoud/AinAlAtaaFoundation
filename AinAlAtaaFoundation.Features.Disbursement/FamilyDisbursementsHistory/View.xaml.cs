dc7100d8abfc99cea1318107810ee0a2090771b9
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-08-25 14:28:48 +0300

    add family disbursements history feature

---
+++ AinAlAtaaFoundation.Features.Disbursement/FamilyDisbursementsHistory/View.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace AinAlAtaaFoundation.Features.DisbursementManagement.FamilyDisbursementsHistory
+{
+    internal partial class View : Window
+    {
+        public View(ViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+            _viewModel = viewModel;
+            Loaded += View_Loaded;
+        }
+
+        private async void View_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Dispatcher.InvokeAsync(_viewModel.LoadDataAsync);
+        }
+
+        private readonly ViewModel _viewModel;
+
+        private void Pagination_PageUpdated(object sender, HandyControl.Data.FunctionEventArgs<int> e)
+        {
+            _viewModel.NextPageCommand.Execute(e.Info - 1);
+        }
+    }
+}
---
+++ AinAlAtaaFoundation.Features.Disbursement/FamilyDisbursementsHistory/ViewModel.cs
@@ -0,0 +1,55 @@
+﻿using AinAlAtaaFoundation.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using MediatR;
+using Syncfusion.Data.Extensions;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AinAlAtaaFoundation.Features.DisbursementManagement.FamilyDisbursementsHistory
+{
+    internal partial class ViewModel(IMediator mediator, IMessenger messenger, Family family) : ObservableObject
+    {
+        private static int _itesmCount = 50;
+
+        public async Task LoadDataAsync()
+        {
+            _allDisbursements = new ObservableCollection<Disbursement>(await _mediator.Send(new Shared.Commands.Disbursements.GetByFamilyId(Family.Id)));
+
+            Pages = (int)Math.Ceiling(_allDisbursements.Count / (double)_itesmCount);
+            OnPropertyChanged(nameof(Pages));
+            Disbursements = _allDisbursements.Skip(0).Take(_itesmCount).ToObservableCollection();
+        }
+
+        [RelayCommand]
+        private Task NextPage(int index)
+        {
+            Disbursements = _allDisbursements.Skip(index * _itesmCount).Take(_itesmCount).ToObservableCollection();
+            return Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private async Task Remove(Disbursement disbursement)
+        {
+            await _mediator.Send(new Shared.Commands.Disbursements.CommandRemove(disbursement));
+            Disbursements.Remove(disbursement);
+            messenger.Send(new Shared.Notifications.SuccessNotification("تم الحذف بنجاح"));
+        }
+
+        public int Pages { get; set; }
+
+        public Family Family { get; private set; } = family;
+
+        public ObservableCollection<Disbursement> Disbursements
+        {
+            get => _disbursements;
+            private set => SetProperty(ref _disbursements, value);
+        }
+        private ObservableCollection<Disbursement> _disbursements;
+        private ObservableCollection<Disbursement> _allDisbursements;
+        private readonly IMediator _mediator = mediator;
+    }
+}
