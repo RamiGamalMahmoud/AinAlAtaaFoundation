2bdcc026d335e259e84b351f905809e35fdffcef
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-08-26 18:15:03 +0300

    Create FamilyDataModelTests.cs

---
+++ AinAlAtaaFoundation.Tests/Families/FamilyDataModelTests.cs
@@ -0,0 +1,65 @@
+﻿using AinAlAtaaFoundation.Features.FamiliesManagement;
+using AinAlAtaaFoundation.Models;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AinAlAtaaFoundation.Tests.Families
+{
+    public class FamilyDataModelTests
+    {
+        [Fact]
+        public void IsOrphan_ShouldBeTrue_WhenScocialStatusIsOrphan()
+        {
+            Family family = new Family()
+            {
+                Address = new Address()
+                {
+                    District = new District(),
+                    FeaturedPoint = new FeaturedPoint()
+                },
+                DistrictRepresentative = new DistrictRepresentative()
+                {
+                    District = new District()
+                },
+                Applicant = new FamilyMember(),
+                SocialStatus = new SocialStatus()
+            };
+
+            FamilyDataModel dataModel = new FamilyDataModel(family);
+
+            dataModel.SocialStatus = new SocialStatus() { Name = "أيتام" };
+
+            dataModel.IsOrphan.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsSponsered_ShouldBeFalse_WhenScocialStatusIsNotOrphan()
+        {
+            Family family = new Family()
+            {
+                Address = new Address()
+                {
+                    District = new District(),
+                    FeaturedPoint = new FeaturedPoint()
+                },
+                DistrictRepresentative = new DistrictRepresentative()
+                {
+                    District = new District()
+                },
+                Applicant = new FamilyMember(),
+                SocialStatus = new SocialStatus(),
+                IsSponsored = true
+            };
+
+            FamilyDataModel dataModel = new FamilyDataModel(family);
+
+            dataModel.SocialStatus = new SocialStatus();
+
+            dataModel.IsSponsered.Should().BeFalse();
+        }
+    }
+}
