a59c5ba26d29a8d0d3e418c24ccab1de22167888
Rami <rami.gamal.mahmoud@gmail.com>
2024-08-30 20:39:14 +0300

    create settings view

---
+++ AinAlAtaaFoundation.Features.Settings/View.xaml.cs
@@ -0,0 +1,14 @@
+﻿using AinAlAtaaFoundation.Shared.Abstraction.Settings;
+using System.Windows;
+
+namespace AinAlAtaaFoundation.Features.Settings
+{
+    internal partial class View : Window, ISettingsView
+    {
+        public View(ViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
