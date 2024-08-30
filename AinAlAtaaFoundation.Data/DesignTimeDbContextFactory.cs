f735ca24db76fbe6e79b53bfbf22deeed719b37d
Rami <rami.gamal.mahmoud@gmail.com>
2024-08-30 23:07:29 +0300

    fix database name in DesignTimeDbContextFactory.cs

--- AinAlAtaaFoundation.Data/DesignTimeDbContextFactory.cs
+++ AinAlAtaaFoundation.Data/DesignTimeDbContextFactory.cs
@@ -10,7 +10,7 @@ namespace AinAlAtaaFoundation.Data
             Console.WriteLine(args.Length.ToString());
             DbContextOptionsBuilder<AppDbContext> builder = new DbContextOptionsBuilder<AppDbContext>();
             builder
-                .UseSqlite("Data Source = .\\DB\\al-ain.db")
+                .UseSqlite("Data Source = .\\DB\\al_ain.db")
                 .UseSnakeCaseNamingConvention();
 
             AppDbContext dbContext = new AppDbContext(builder.Options);
