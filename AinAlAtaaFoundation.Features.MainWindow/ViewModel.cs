e18dc8dbb6a83d8f0c16e8009ea7b534269cde1c
Rami <rami.gamal.mahmoud@gmail.com>
2024-08-30 20:40:25 +0300

    remove database commands

--- AinAlAtaaFoundation.Features.MainWindow/ViewModel.cs
+++ AinAlAtaaFoundation.Features.MainWindow/ViewModel.cs
@@ -30,15 +30,6 @@ namespace AinAlAtaaFoundation.Features.MainWindow
         [ObservableProperty]
         private object _currentView;
 
-        [RelayCommand]
-        private void Backup() => _messenger.Send<Messages.Database.BackupMessage>();
-
-        [RelayCommand]
-        private void Restore() => _messenger.Send<Messages.Database.ResoreMessage>();
-
-        [RelayCommand]
-        private void ResetDatabase() => _messenger.Send<Messages.Database.ResetMessage>();
-
         public static string LogoPath => Path.Combine(Environment.CurrentDirectory, "Images\\logo-100.png");
 
         public IAppState AppState { get; }
