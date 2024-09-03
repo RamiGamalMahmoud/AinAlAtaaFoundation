a5746621d2e5abf31a5c9a1366e016f9ad74fdd0
Rami <rami.gamal.mahmoud@gmail.com>
2024-09-03 20:39:36 +0300

    add statistics components

---
+++ AinAlAtaaFoundation.Features.MainWindow/Statistics/Components/HeaderItem.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AinAlAtaaFoundation.Features.MainWindow.Statistics.Components
+{
+    public partial class HeaderItem : UserControl
+    {
+        public HeaderItem()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get => (string)GetValue(_titmeProperty);
+            set => SetValue(_titmeProperty, value);
+        }
+        private static DependencyProperty _titmeProperty =
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(HeaderItem),
+                new PropertyMetadata(null)
+                );
+
+        public int Value
+        {
+            get => (int)GetValue(_valueProperty);
+            set => SetValue(_valueProperty, value);
+        }
+        private static DependencyProperty _valueProperty =
+            DependencyProperty.Register(
+                nameof(Value),
+                typeof(int),
+                typeof(HeaderItem),
+                new PropertyMetadata(null)
+                );
+    }
+}
---
+++ AinAlAtaaFoundation.Features.MainWindow/Statistics/Components/StatisticItem.xaml.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AinAlAtaaFoundation.Features.MainWindow.Statistics.Components
+{
+    [ObservableObject]
+    public partial class StatisticItem : UserControl
+    {
+        public StatisticItem()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get => (string)GetValue(_titmeProperty);
+            set => SetValue(_titmeProperty, value);
+        }
+        private static DependencyProperty _titmeProperty =
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(StatisticItem),
+                new PropertyMetadata(null)
+                );
+
+        public IEnumerable<Item> Items
+        {
+            get => (IEnumerable<Item>)GetValue(_itemsProperty);
+            set => SetValue(_itemsProperty, value);
+        }
+        private static DependencyProperty _itemsProperty =
+            DependencyProperty.Register(
+                nameof(Items),
+                typeof(IEnumerable<Item>),
+                typeof(StatisticItem),
+                new PropertyMetadata(null)
+                );
+
+    }
+}
