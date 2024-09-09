4cc99c3b9304981894f26135f453e948a47e946f
Rami <rami.gamal.mahmoud@gmail.com>
2024-09-09 21:27:04 +0300

    add ExportPdfCommandHandler

---
+++ AinAlAtaaFoundation.Services.Printing/ExportPdfCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using AinAlAtaaFoundation.Shared.Commands;
+using MediatR;
+using Microsoft.Reporting.WinForms;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AinAlAtaaFoundation.Services.Printing
+{
+    internal class ExportPdfCommandHandler : IRequestHandler<Shared.Commands.Generic.ExportToPdfCommand, string>
+    {
+        public async Task<string> Handle(Generic.ExportToPdfCommand request, CancellationToken cancellationToken)
+        {
+            string reportPath = System.IO.Path.Combine(Environment.CurrentDirectory, "Reports", request.ReportName);
+            LocalReport localReport = LocalReportHelpers.CreateLocalReport(reportPath, request.Parameters, request.DataSources);
+            return await LocalReportHelpers.Render(localReport, request.OutputFileName, "PDF", "pdf");
+        }
+    }
+}
