fae2d5cc51ced6e75fc9634a257cbc866ae78767
Rami <rami.gamal.mahmoud@gmail.com>
2024-09-09 21:26:53 +0300

    add ExportToExcelCommandCommandHandler class

---
+++ AinAlAtaaFoundation.Services.Printing/ExportToExcelCommandCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.Reporting.WinForms;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using static AinAlAtaaFoundation.Shared.Commands.Generic;
+
+namespace AinAlAtaaFoundation.Services.Printing
+{
+    internal class ExportToExcelCommandCommandHandler : IRequestHandler<ExportToExcelCommand>
+    {
+        public async Task Handle(ExportToExcelCommand request, CancellationToken cancellationToken)
+        {
+            string reportPath = System.IO.Path.Combine(Environment.CurrentDirectory, "Reports", request.ReportName);
+            LocalReport localReport = LocalReportHelpers.CreateLocalReport(reportPath, request.Parameters, request.DataSources);
+            await LocalReportHelpers.Render(localReport, request.OutputFileName, "EXCELOPENXML", "xlsx");
+        }
+    }
+}
