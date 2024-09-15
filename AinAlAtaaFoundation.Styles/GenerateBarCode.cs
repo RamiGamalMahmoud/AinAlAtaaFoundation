9de6987d6145462e97438d12655f0745a6469a7c
Rami <rami.gamal.mahmoud@gmail.com>
2024-09-15 15:56:09 +0300

    use default colors and size for barcode generation

--- AinAlAtaaFoundation.Styles/GenerateBarCode.cs
+++ AinAlAtaaFoundation.Styles/GenerateBarCode.cs
@@ -24,7 +24,7 @@ namespace AinAlAtaaFoundation.Shared
                 IncludeLabel = true
             };
             return Image
-                .FromStream(b.Encode(BarcodeStandard.Type.Code128, code.ToString(), SKColors.Black, SKColors.White, 290, 120)
+                .FromStream(b.Encode(BarcodeStandard.Type.Code128, code.ToString(), SKColors.Black, SKColors.White)
                 .Encode()
                 .AsStream());
         }
