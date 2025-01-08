b2fcd60b115b8e1507f0526ede2186bf9858c3bc
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-01-08 02:31:27 +0200

    Create CreateCommandHandler.cs

---
+++ AinAlAtaaFoundation.Features.Management/AssociationRepresentatives/CommandHandlers/CreateCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using AinAlAtaaFoundation.Models;
+using AinAlAtaaFoundation.Shared.Commands;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AinAlAtaaFoundation.Features.Management.AssociationRepresentatives.CommandHandlers
+{
+    internal class CreateCommandHandler(Repository repository) : IRequestHandler<Generic.CommandCreate<AssociationRepresentative, AssociationRepresentativeDataModel>, AssociationRepresentative>
+    {
+        public async Task<AssociationRepresentative> Handle(Generic.CommandCreate<AssociationRepresentative, AssociationRepresentativeDataModel> request, CancellationToken cancellationToken)
+        {
+            return await repository.Create(request.DataModel);
+        }
+    }
+}
