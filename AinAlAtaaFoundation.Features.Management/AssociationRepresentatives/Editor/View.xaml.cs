015eba4b715e6149adb2fc3a60bd6da5a6afeead
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-01-08 02:31:43 +0200

    Create View.xaml.cs

---
+++ AinAlAtaaFoundation.Features.Management/AssociationRepresentatives/Editor/View.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace AinAlAtaaFoundation.Features.Management.AssociationRepresentatives.Editor
+{
+    internal partial class View : Window
+    {
+        public View(ViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
