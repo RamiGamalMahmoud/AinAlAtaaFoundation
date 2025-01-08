d1e64a456b882b99e2c123b464bfb749dd41c7b9
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-01-08 02:31:32 +0200

    Create GetAllCommandHandler.cs

---
+++ AinAlAtaaFoundation.Features.Management/AssociationRepresentatives/CommandHandlers/GetAllCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using AinAlAtaaFoundation.Models;
+using AinAlAtaaFoundation.Shared.Commands;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AinAlAtaaFoundation.Features.Management.AssociationRepresentatives.CommandHandlers
+{
+    internal class GetAllCommandHandler(Repository repository) : IRequestHandler<Generic.GetAllCommand<AssociationRepresentative>, IEnumerable<AssociationRepresentative>>
+    {
+        public async Task<IEnumerable<AssociationRepresentative>> Handle(Generic.GetAllCommand<AssociationRepresentative> request, CancellationToken cancellationToken)
+        {
+            return await repository.GetAll(request.Reload);
+        }
+    }
+}
