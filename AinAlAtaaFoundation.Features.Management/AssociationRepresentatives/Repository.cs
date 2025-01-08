63f0f1a1f91fb095e0816dc295b463d00ecf9eed
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-01-08 02:31:53 +0200

    Create Repository.cs

---
+++ AinAlAtaaFoundation.Features.Management/AssociationRepresentatives/Repository.cs
@@ -0,0 +1,55 @@
+﻿using AinAlAtaaFoundation.Data;
+using AinAlAtaaFoundation.Models;
+using AinAlAtaaFoundation.Shared.Abstraction;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AinAlAtaaFoundation.Features.Management.AssociationRepresentatives
+{
+    internal class Repository(AppDbContextFactory dbContextFactory) : RepositoryBase<AssociationRepresentative, AssociationRepresentativeDataModel>(dbContextFactory)
+    {
+        public override async Task<AssociationRepresentative> Create(AssociationRepresentativeDataModel dataModel)
+        {
+            using (AppDbContext dbContext = _dbContextFactory.CreateDbContext())
+            {
+                AssociationRepresentative representative = new AssociationRepresentative
+                {
+                    Name = dataModel.Name
+                };
+                dbContext.AssociationRepresentatives.Add(representative);
+                try
+                {
+                    await dbContext.SaveChangesAsync();
+                    dataModel.UpdateModel(representative);
+                    _entities.Add(representative);
+                    return representative;
+
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public override async Task<IEnumerable<AssociationRepresentative>> GetAll(bool reload = false)
+        {
+            if (!_isLoaded || reload)
+            {
+                using (AppDbContext dbContext = _dbContextFactory.CreateDbContext())
+                {
+                    IEnumerable<AssociationRepresentative> representatives = await dbContext.AssociationRepresentatives.ToListAsync();
+                    SetEntities(representatives);
+                }
+            }
+            return _entities;
+        }
+
+        public override Task<bool> Update(AssociationRepresentativeDataModel dataModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
