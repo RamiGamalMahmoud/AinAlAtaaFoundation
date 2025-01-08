71465790217a03dd0fd63d0e24d3311683ed6fdd
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-01-08 02:31:36 +0200

    Create ShowCreateCommandHandler.cs

---
+++ AinAlAtaaFoundation.Features.Management/AssociationRepresentatives/CommandHandlers/ShowCreateCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using AinAlAtaaFoundation.Features.Management.AssociationRepresentatives.Editor;
+using AinAlAtaaFoundation.Models;
+using AinAlAtaaFoundation.Shared.Commands;
+using CommunityToolkit.Mvvm.Messaging;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AinAlAtaaFoundation.Features.Management.AssociationRepresentatives.CommandHandlers
+{
+    internal class ShowCreateCommandHandler(IServiceProvider services) : IRequestHandler<Generic.ShowCreate<AssociationRepresentative>>
+    {
+        public Task Handle(Generic.ShowCreate<AssociationRepresentative> request, CancellationToken cancellationToken)
+        {
+            ViewModelCreate viewModel = new ViewModelCreate(services.GetRequiredService<IMediator>(), services.GetRequiredService<IMessenger>());
+            Editor.View view = new Editor.View(viewModel);
+            view.ShowDialog();
+            return Task.CompletedTask;
+        }
+    }
+}
