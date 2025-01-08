c570884bd626ccde16b82a09030c6c2e976a6294
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-01-08 02:31:49 +0200

    Create ViewModelCreate.cs

---
+++ AinAlAtaaFoundation.Features.Management/AssociationRepresentatives/Editor/ViewModelCreate.cs
@@ -0,0 +1,30 @@
+﻿using AinAlAtaaFoundation.Models;
+using AinAlAtaaFoundation.Shared.Commands;
+using CommunityToolkit.Mvvm.Messaging;
+using MediatR;
+using System.Threading.Tasks;
+
+namespace AinAlAtaaFoundation.Features.Management.AssociationRepresentatives.Editor
+{
+    internal class ViewModelCreate : ViewModel
+    {
+        public ViewModelCreate(IMediator mediator, IMessenger messenger) : base(mediator, messenger, null)
+        {
+        }
+
+        protected override async Task Save()
+        {
+            AssociationRepresentative representative = await _mediator.Send(new Generic.CommandCreate<AssociationRepresentative, AssociationRepresentativeDataModel>(DataModel));
+            if(representative is null)
+            {
+                _messenger.Send(new Shared.Notifications.FailerNotification("خطأ"));
+            }
+            else
+            {
+                _messenger.Send(new Shared.Messages.EntityCreated<AssociationRepresentative>(DataModel.Model));
+                _messenger.Send(new Shared.Notifications.SuccessNotification("تم الحفظ بنجاح"));
+                DataModel.Name = "";
+            }
+        }
+    }
+}
