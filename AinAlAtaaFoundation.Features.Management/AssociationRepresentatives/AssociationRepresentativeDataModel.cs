77789b1433fabbe87f391b7a185ff8e473a42fc4
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-01-08 02:31:23 +0200

    Create AssociationRepresentativeDataModel.cs

---
+++ AinAlAtaaFoundation.Features.Management/AssociationRepresentatives/AssociationRepresentativeDataModel.cs
@@ -0,0 +1,36 @@
+﻿using AinAlAtaaFoundation.Models;
+using AinAlAtaaFoundation.Shared.Abstraction;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AinAlAtaaFoundation.Features.Management.AssociationRepresentatives
+{
+    internal partial class AssociationRepresentativeDataModel : ObservableValidator, IDataModel<AssociationRepresentative>
+    {
+        public AssociationRepresentativeDataModel(AssociationRepresentative model)
+        {
+            if (model is not null)
+            {
+                Model = model;
+                Name = model.Name;
+            }
+            ValidateAllProperties();
+        }
+
+        [ObservableProperty]
+        [Required(ErrorMessage = "حقل مطلوب")]
+        [NotifyDataErrorInfo]
+        [NotifyPropertyChangedFor(nameof(IsValid))]
+        private string _name;
+
+        public bool IsValid => !HasErrors;
+
+        public AssociationRepresentative Model { get; private set; }
+
+        public void UpdateModel(AssociationRepresentative model = null)
+        {
+            model.Name = Name;
+        }
+    }
+}
