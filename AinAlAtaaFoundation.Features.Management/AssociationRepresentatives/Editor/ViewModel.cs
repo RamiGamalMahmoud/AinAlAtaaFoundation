2c678d023dc8fb701c2c1d72d94ef61271f8a827
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-01-08 02:31:46 +0200

    Create ViewModel.cs

---
+++ AinAlAtaaFoundation.Features.Management/AssociationRepresentatives/Editor/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using AinAlAtaaFoundation.Models;
+using AinAlAtaaFoundation.Shared;
+using CommunityToolkit.Mvvm.Messaging;
+using MediatR;
+using System;
+using System.Threading.Tasks;
+
+namespace AinAlAtaaFoundation.Features.Management.AssociationRepresentatives.Editor
+{
+    internal abstract class ViewModel : ViewModelBase<AssociationRepresentativeDataModel>
+    {
+        public ViewModel(IMediator mediator, IMessenger messenger, AssociationRepresentative model) : base(mediator, messenger)
+        {
+            DataModel = new AssociationRepresentativeDataModel(model);
+            HasChangesObject = new Shared.Helpers.HasChangesObject(SaveCommand.NotifyCanExecuteChanged);
+            DataModel.PropertyChanged += DataModel_PropertyChanged;
+        }
+
+        public override bool CanSave() => HasChangesObject.HasChanges && DataModel.IsValid;
+
+
+        public override Task LoadDataAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
